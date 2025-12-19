85e6120cc07d43339d229aa7beccdb6675b8dfa2
balajigood3 <good4all1989@gmail.com>
2025-12-19 11:27:14 +0530

    Refactor Extra_Assignment-2.py with additional features

--- Extra_Assignment-2.py
+++ Extra_Assignment-2.py
@@ -1,3 +1,7 @@
+#Authour : Balaji M
+# Date : 17-12-2025
+# Extra Assignment-2
+
 # print 0 to 20 by using range
 for i in range(0, 21):
     print(i)
@@ -5,9 +9,40 @@ for i in range(0, 21):
 # print range 10 to 20
 for i in range(10, 21):
     print(i)
+
 # Print number of items in the list by using 'len'
 List2= [10, 20, 14, 55, 43, 87, 76]
 print("List2:")
 print(List2)
 print("Number of item in the List2:")
 print(len(List2))
+
+#print Chart Artificial intellegance
+for chart in range(len("Artificial Intelligence")):
+       print ("Artificial Intelligence"[chart])
+
+# Print Statement
+print("-Your Name-")
+print("-Your Age-")
+print("-Your Profession-")
+
+# Print this mixer datatype using Tuples
+Mixer_Tuple1=(1, 'Welcome', 2, 'Hope')
+print(Mixer_Tuple1)
+
+# Print two different Tuples in a single print statement
+Tuple1 =(0,1,2,3)
+Tuple2 =('python', 'HOPE')
+print((Tuple1, Tuple2))
+
+# Print Odd Numbers in the list 
+List = (20,10,16,19,25,1,276,188)
+for num in List:
+    if num % 2 != 0:
+        print(num, 'is odd')
+
+# print Even numbers in the list
+List = (20,10,16,19,25,1,276,188)
+for num in List:
+    if num % 2 == 0:
+        print(num, 'is even')
